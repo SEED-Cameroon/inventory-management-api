596c5b1a7a8194cbdf0301534fff8c9652d9b91c
Venom-bot460 <sharrellaure@gmail.com>
2026-08-04 14:18:23 +0100

    Created project folder,and files

---
+++ models/Supplier.js
@@ -0,0 +1,26 @@
+// import mongoose from "mongoose";
+
+// const supplierSchema = new mongoose.Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//     },
+
+//     contactEmail: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//       lowercase: true,
+//       trim: true,
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// const Supplier = mongoose.model("Supplier", supplierSchema);
+
+// export default Supplier;
