27aab6e4299052be629bbc9af9f29d96d97241ee
Venom-bot460 <sharrellaure@gmail.com>
2026-08-05 12:53:45 +0100

    completed work on models

--- models/RestockEvent.js
+++ models/RestockEvent.js
@@ -1,29 +1,29 @@
 import mongoose from "mongoose";
 
-const restockSchema = new mongoose.Schema(
-  {
-    productId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Product",
-      required: true,
-    },
-
-    quantity: {
-      type: Number,
-      required: true,
-      min: 1,
-    },
-
-    supplierId: {
-       type: mongoose.Schema.Types.ObjectId,
-      ref: "Supplier",
-      required: true,
-    },
+const restockSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
   },
-  {
-    timestamps: true,
-  }
-);
+
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+
+  supplierId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Supplier",
+    required: true,
+  },
+
+  receivedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 const Restock = mongoose.model("Restock", restockSchema);
 
